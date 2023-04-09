ab7002f14905e767d8af10573e3fe3fee3a036fc
lakhalEya <lakhal.eya@esprit.tn>
2023-04-09 21:40:46 +0100

    Essai 3 : Automatic launch with specifying Webhooks

--- src/main/java/tn/exam/exam/aop/LoggingAspect.java
+++ src/main/java/tn/exam/exam/aop/LoggingAspect.java
@@ -2,7 +2,6 @@ package tn.exam.exam.aop;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
